c8f1ee52fe03b9966f3b98a9fad5f7472d353779
Hamcha <zikyky@gmail.com>
2015-06-18 12:23:37 +0200

    Added GeoIP checking

--- Makefile
+++ Makefile
@@ -13,6 +13,7 @@ run: all
 dep:
 	go get github.com/gorilla/mux
 	go get github.com/microcosm-cc/bluemonday
+	go get github.com/oschwald/maxminddb-golang
 	go get gopkg.in/mgo.v2
 	touch dep
 
--- src/blacklist.go
+++ src/blacklist.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"./data"
+	"github.com/oschwald/maxminddb-golang"
 	"log"
+	"net"
 	"net/http"
 	"regexp"
 )
@@ -20,27 +22,38 @@ type Blacklist struct {
 
 var blacklists map[string]Blacklist
 var captchas []data.CaptchaData
+var geoip *maxminddb.Reader
+
+type onlyCountry struct {
+	Country struct {
+		IsoCode string `maxminddb:"iso_code"`
+	} `maxminddb:"country"`
+}
 
 func initBL() {
 	// Load Blacklist conf
 	err := LoadJson("blacklist.conf", &blacklists)
 	if err != nil {
 		log.Printf("[ WARNING ] %s/blacklist.conf not found. BL not initialized.\n", maudRoot)
-		return
-	}
-
-	for i := range blacklists {
-		item := blacklists[i]
-		item.IPRegexp = regexp.MustCompile(blacklists[i].IP)
-		item.UARegexp = regexp.MustCompile(blacklists[i].UserAgent)
-		blacklists[i] = item
+	} else {
+		for i := range blacklists {
+			item := blacklists[i]
+			item.IPRegexp = regexp.MustCompile(blacklists[i].IP)
+			item.UARegexp = regexp.MustCompile(blacklists[i].UserAgent)
+			blacklists[i] = item
+		}
 	}
 
 	// Load Captcha conf
 	err = LoadJson("captcha.conf", &captchas)
 	if err != nil {
-		log.Printf("[ WARNING %s/captcha.conf not found. Captchas not initialized.\n", maudRoot)
-		return
+		log.Printf("[ WARNING ] %s/captcha.conf not found. Captchas not initialized.\n", maudRoot)
+	}
+
+	// Load GeoIP database
+	geoip, err = maxminddb.Open("geoip.mmdb")
+	if err != nil {
+		log.Printf("[ WARNING ] %s/geoip.mmdb not found. GeoIP limiting not initialized.\n", maudRoot)
 	}
 }
 
@@ -54,18 +67,33 @@ func checkBlacklist(req *http.Request) (bool, string, string) {
 	} else {
 		ip = req.RemoteAddr
 	}
-	for i := range blacklists {
-		ipmatch := blacklists[i].IPRegexp.MatchString(ip)
-		uamatch := blacklists[i].UARegexp.MatchString(userAgent)
-		var matches bool
-		if blacklists[i].Criteria == "ALL" {
-			matches = ipmatch && uamatch
-		} else {
-			matches = ipmatch || uamatch
-		}
 
-		if matches {
-			return true, blacklists[i].Reason, blacklists[i].Action
+	// Check on blacklist.conf
+	if blacklists != nil {
+		for i := range blacklists {
+			ipmatch := blacklists[i].IPRegexp.MatchString(ip)
+			uamatch := blacklists[i].UARegexp.MatchString(userAgent)
+			var matches bool
+			if blacklists[i].Criteria == "ALL" {
+				matches = ipmatch && uamatch
+			} else {
+				matches = ipmatch || uamatch
+			}
+
+			if matches {
+				return true, blacklists[i].Reason, blacklists[i].Action
+			}
+		}
+	}
+
+	// Check on GeoIP database
+	if geoip != nil {
+		ip := net.ParseIP(ip)
+		var result onlyCountry
+		geoip.Lookup(ip, &result)
+
+		if result.Country.IsoCode != "IT" {
+			return true, "", "captcha"
 		}
 	}
 
