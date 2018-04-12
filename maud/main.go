c3da7572f64caad067bda9bdcc60f704ddb899d3
silverweed <silverweed1991@gmail.com>
2018-04-12 13:14:19 +0200

    remove google fonts from CSP rules

--- maud/main.go
+++ maud/main.go
@@ -88,8 +88,8 @@ func main() {
 	// Setup CSP
 	csp = map[string]string{
 		"script-src": "'self'",
-		"style-src":  "'self' https://fonts.googleapis.com/",
-		"font-src":   "'self' https://fonts.gstatic.com/",
+		"style-src":  "'self'",
+		"font-src":   "'self'",
 		//"object-src": "'none'",
 	}
 
