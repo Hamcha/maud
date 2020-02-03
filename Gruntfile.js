7095597d99a4ecd20c70befef6bf32bca2376af3
Hamcha <hamcha@crunchy.rocks>
2020-02-03 11:37:48 +0100

    Move from compass to dart-sass

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,114 +1,106 @@
-'use strict';
+"use strict";
 
-module.exports = function (grunt) {
-	require('time-grunt')(grunt);
-	require('load-grunt-tasks')(grunt);
+module.exports = function(grunt) {
+  require("time-grunt")(grunt);
+  require("load-grunt-tasks")(grunt);
 
-	grunt.initConfig({
-		yeoman: {
-            scss: 'static/src/scss',
-            css: 'static/css',
-            coffee: 'static/src/coffee',
-            js: 'static/js',
-            images: 'static/images'
-        },
+  grunt.initConfig({
+    yeoman: {
+      scss: "static/src/scss",
+      css: "static/css",
+      coffee: "static/src/coffee",
+      js: "static/js",
+      images: "static/images"
+    },
 
-        watch: {
-        	coffeeweb: {
-                files: ['<%= yeoman.coffee %>/{,*/}*.coffee'],
-                tasks: ['coffee:web']
-            },
-            compass: {
-                files: ['<%= yeoman.scss %>/{,*/}*.{scss,sass}'],
-                tasks: ['compass:server']
-            },
-            livereload: {
-                options: {
-                    livereload: '<%= connect.options.livereload %>'
-                },
-                files: [
-                    '{,*/}*.html',
-                    '<%= yeoman.css %>{,*/}*.css',
-                    '<%= yeoman.images %>/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
-                    '<%= yeoman.js %>/{,*/}*.js'
-                ]
-            }
+    watch: {
+      coffeeweb: {
+        files: ["<%= yeoman.coffee %>/{,*/}*.coffee"],
+        tasks: ["coffee:web"]
+      },
+      compass: {
+        files: ["<%= yeoman.scss %>/{,*/}*.{scss,sass}"],
+        tasks: ["compass:server"]
+      },
+      livereload: {
+        options: {
+          livereload: "<%= connect.options.livereload %>"
         },
-        connect: {
-            options: {
-                port: 9000,
-                livereload: 35729,
-                // change this to '0.0.0.0' to access the server from outside
-                hostname: 'localhost'
-            },
-            livereload: {
-                options: {
-                    open: true,
-                    base: ['.']
-                }
-            }
-        },
-        coffee: {
-            web: {
-                files: [{
-                    expand: true,
-                    cwd: '<%= yeoman.coffee %>',
-                    src: '{,*/}*.coffee',
-                    dest: '<%= yeoman.js %>',
-                    ext: '.js',
-                }],
-                options: {
-                    sourceMap: true
-                }
-            }
-        },
-        compass: {
-            options: {
-                sassDir: '<%= yeoman.scss %>',
-                cssDir: '<%= yeoman.css %>',
-                generatedImagesDir: '<%= yeoman.images %>/generated',
-                imagesDir: '<%= yeoman.images %>',
-                javascriptsDir: '<%= yeoman.js %>',
-                fontsDir: 'fonts',
-                importPath: '/',
-                httpImagesPath: '/<%= yeoman.images %>',
-                httpGeneratedImagesPath: '/<%= yeoman.images %>/generated',
-                httpFontsPath: '/fonts',
-                relativeAssets: false,
-                assetCacheBuster: false
-            },
-            dist: {
-                options: {
-                    generatedImagesDir: '<%= yeoman.images %>/generated'
-                }
-            },
-            server: {
-                options: {
-                    debugInfo: false
-                }
-            }
-        },
-        uglify: {
-            options: {
-                mangle: false
-            },
-            all: {
-                src : 'static/js/*.js',
-                dest : 'static/build.js'
-            }
-        },
-        open: {
-          server: {
-            path: 'http://localhost:<%= connect.options.port %>'
-          }
-        },
-        concurrent: {
-            server: [
-                'compass:server',
-                'coffee:web'
-            ]
+        files: [
+          "{,*/}*.html",
+          "<%= yeoman.css %>{,*/}*.css",
+          "<%= yeoman.images %>/{,*/}*.{png,jpg,jpeg,gif,webp,svg}",
+          "<%= yeoman.js %>/{,*/}*.js"
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 9000,
+        livereload: 35729,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: "localhost"
+      },
+      livereload: {
+        options: {
+          open: true,
+          base: ["."]
         }
-	});
-    grunt.registerTask('server', ['concurrent:server', 'connect:livereload', 'watch']);
-    grunt.registerTask('build', ['coffee:web', 'compass:dist']);
-}
+      }
+    },
+    coffee: {
+      web: {
+        files: [
+          {
+            expand: true,
+            cwd: "<%= yeoman.coffee %>",
+            src: "{,*/}*.coffee",
+            dest: "<%= yeoman.js %>",
+            ext: ".js"
+          }
+        ],
+        options: {
+          sourceMap: true
+        }
+      }
+    },
+    "dart-sass": {
+      dist: {
+        files: [
+          {
+            expand: true,
+            cwd: ".",
+            src: ["<%= yeoman.scss %>/*.scss"],
+            dest: "<%= yeoman.css %>",
+            ext: ".css"
+          }
+        ]
+      }
+    },
+    uglify: {
+      options: {
+        mangle: false
+      },
+      all: {
+        src: "static/js/*.js",
+        dest: "static/build.js"
+      }
+    },
+    open: {
+      server: {
+        path: "http://localhost:<%= connect.options.port %>"
+      }
+    },
+    concurrent: {
+      server: ["dart-sass", "coffee:web"]
+    }
+  });
+  grunt.loadNpmTasks("grunt-dart-sass");
+
+  grunt.registerTask("server", [
+    "concurrent:server",
+    "connect:livereload",
+    "watch"
+  ]);
+  grunt.registerTask("build", ["coffee:web", "dart-sass:dist"]);
+};
